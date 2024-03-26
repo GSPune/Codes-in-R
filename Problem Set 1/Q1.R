79015cf242870860ab96f0529f30b3ab78b5fc56
Stephen George <stephengeorgepune@gmail.com>
2024-03-26 18:44:08 +0530

    Calculating Median of Grouped Data Distribution

--- Problem Set 1/Q1.R
+++ Problem Set 1/Q1.R
@@ -4,9 +4,14 @@
 LC = seq(0,600,100)
 UC = seq(100,700,100)
 
-f = c(9,15,18,21,18,14,5)
-data = data.frame(LC,UC,f)
+#test data
+# LC = seq(0.5,80.5,20)
+# UC = seq(20.5,100.5,20)
 
+f = c(9,15,18,21,18,14,5)
+# f = c(1,9,32,16,7)
+data = data.frame(LC,UC,f)
+#-----------------------------------------------------------------------------------------------------
 #(a) Arithmetic mean: The results must include a table with classmarks and product
 #of classmarks and frequencies.
 
@@ -15,8 +20,9 @@ xf = x * f
 data2 = data.frame(LC,UC,f,x,xf)
 data2
 arth_mean = sum(xf)/sum(f)
-
+#-----------------------------------------------------------------------------------------------------
 #(b) Median: The results must include a table containing LCF.
+N = sum(f)
 LCF<-integer(length(f))
 LCF[1] = f[1]
 for (i in 2:length(f)){
@@ -24,5 +30,17 @@ for (i in 2:length(f)){
     LCF[i] = LCF[i-1]+f[i]
 }
 
+for (i in 2:length(f)){
+    if(LCF[i] > (N/2)){
+        p = i;break; #p gives us the median class row 
+    }
+}
+
 data3 = cbind(data,LCF)
-    
+l = data3[p,1] #lower class boundary of median class
+cf = data3[p-1,4] #Lcf of class previous to median class
+F0 = data3[p,3] # freq. of median class
+h = (UC - LC)[1] #class width of median class
+Median  = l + (((N/2)-cf)*(h/F0))
+print(Median)
+#----------------------------------------------------------------------------------------------------
