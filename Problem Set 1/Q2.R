f58f86eae4739a410329973cc1bf7cf084f38c19
Stephen George <stephengeorgepune@gmail.com>
2024-03-27 19:53:23 +0530

    Added functions for Mode and Quartiles

--- Problem Set 1/Q2.R
+++ Problem Set 1/Q2.R
@@ -19,3 +19,48 @@ mean=function(LC,UC,f)
     return(arth_mean)
 }
 print(paste("Mean :",mean(l,u,fq)))
+#---------------------------------------------------------------------------------------------------
+#Function for calculation of Mode
+mode = function(LC,UC,f)
+{
+    max = 1
+    
+    for (i in 1:length(f)){
+        if(f[i] > f[max]){
+            max = i; #max gives us the modal class row 
+        }
+    }
+    
+    fm = data[max,3]
+    f1 = data[max-1,3] #pre-modal frequency
+    f2 = data[max+1,3] #post-modal frequency
+    
+    l = data[max,1]
+    Mode = l + (h * ((fm - f1)/(2*fm-f1-f2)))
+    return(Mode)
+}
+print(paste("Mode :",mode(l,u,fq)))
+#---------------------------------------------------------------------------------------------------
+#Function for calculation of Quartiles
+#k <- readline("Enter the Quartile number to be calculated:")
+
+Quartiles <- function(LC,UC,f,k){
+    
+    for (i in 2:length(f)){
+        if(LCF[i] > (k*N/4)){
+            u = i;break; #t gives us the Qk class row 
+        }
+    }
+    
+    l = data3[u,1] #lower class boundary of quartile class
+    cf = data3[u-1,4] #Lcf of class previous to quartile class
+    F0 = data3[u,3] # freq. of quartile class
+    h = (UC - LC)[1] #class width of quartile class
+    Qk = l + ((((k*N)/4)-cf)*(h/F0))
+    return(Qk)
+    #print(paste("Quartile :",Qk)
+}
+
+print(paste("1st Quartile: ",Quartiles(l,u,fq,1)))
+print(paste("2nd Quartile: ",Quartiles(l,u,fq,2)))
+print(paste("3rd Quartile: ",Quartiles(l,u,fq,3)))
