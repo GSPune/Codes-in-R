e1a8d07d88382a846c12e6573e541bad397f2db7
Stephen George <stephengeorgepune@gmail.com>
2024-03-27 18:25:14 +0530

    function for arithmetic mean

--- Problem Set 1/Q2.R
+++ Problem Set 1/Q2.R
@@ -3,3 +3,19 @@
 # upper bounds and frequencies as their input arguments and must return the value of
 # a measure of central tendency. Remember, your code must be flexible enough
 # to work with all possible number of classes.
+
+l = seq(0,600,100)
+u = seq(100,700,100)
+
+
+fq = c(9,15,18,21,18,14,5)
+
+mean=function(LC,UC,f)
+{
+    # data = data.frame(LC,UC,f)
+    x = (LC+UC)/2
+    xf = x * f
+    arth_mean = sum(xf)/sum(f)
+    return(arth_mean)
+}
+print(paste("Mean :",mean(l,u,fq)))
