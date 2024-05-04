f2a168821b928a3cbf2d75c0e212d0b9d2193a3a
Stephen <84006256+GSPune@users.noreply.github.com>
2024-05-05 01:28:54 +0530

    Created Q4.R

---
+++ Problem Set 1/Q4.R
@@ -0,0 +1,5 @@
+#Q.(4) Write the separate R codes, dedicated functions for the following measures of dispersion. In the separate codes make a provision to display the tables with products,
+#cross-products and LCF whenever it is necessary. Further, improvise these codes to write a unified function for the measures of dispersion.
+#(a) Quartile deviation and its coefficient.
+#(b) Mean deviation about mean and its coefficient.
+#(c) Variance and coefficient of variation
