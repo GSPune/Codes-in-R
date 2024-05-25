c16d498694db820e72bc8a94cf9fded083b0f8a9
Stephen George <stephengeorgepune@gmail.com>
2024-05-25 15:41:21 +0530

    Minor modification

--- Basics -- Stats,Matrices/Sterling's_Number.R
+++ Basics -- Stats,Matrices/Sterling's_Number.R
@@ -1,26 +1,28 @@
 #Sterling's Number using Recursion
+
 #!/usr/bin/env Rscript
 
 S <- function(n,k){
-      #print(n)
-      #Base Cases!
-      if (k == 1 || k == 0){
-         return (k);
-      }
-
-      if (k == n){
-         return (1);
-      }
-         
-      return (S(n-1,k-1) + k * S(n-1,k));
+#print(n)
+#print(k)
+ if (k == 1 || k == 0){
+   return (k);
 }
 
+if (k == n){
+   return (1);
+}
+   
+ return (S(n-1,k-1) + k * S(n-1,k));
+}
+
+row <- readLines("stdin",n=1)
+cat("Entered value is ::",row,"\n")
 #print(S(4,2))
-for (i in 1:5){
-   for (j in 0:i){
-      #paste(c(S(i,j)))
-      #print(S(i,j))
-      cat(S(i,j))
-   }
-   cat("\n")
+for (i in 1:row){
+ for (j in 0:i){
+    cat(S(i,j))
+    cat(" ")
+}
+cat("\n")
 }
